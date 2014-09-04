886ecc730690b5f94db649970c9fb044e3fd761a
Martin Mágr <mmagr@redhat.com>
2014-09-04 17:05:23 +0200

    Patches

--- commands/cmd_git.py
+++ commands/cmd_git.py
@@ -8,7 +8,7 @@ from kobo.shortcuts import run
 from borga.lib.connectors import BZConnector
 
 
-def format_submodules(print_output=True):
+def format_submodules(installdir, print_output=True):
     """Returns formated git module urls in GitHub format and current commit
     hashes for SPEC file.
     """
@@ -20,10 +20,15 @@ def format_submodules(print_output=True):
     for line in out.split('\n'):
         cont = line.strip().split('=')
         if len(cont) < 2:
+            if line:
+                sys.stdout.write('Invalid line: %s\n' % line)
             continue
         mod = {}
         mod['raw_url'] = cont[1].strip()
-        mod['base_url'] = mod['raw_url'].rstrip('.git')
+        mod['base_url'] = (mod['raw_url'].endswith('.git')
+                                and mod['raw_url'][:-4]
+                                or mod['raw_url'])
+        mod['fullname'] = mod['base_url'].split('/')[-1]
         modules.append(mod)
 
     # get path for modules and github url
@@ -36,42 +41,77 @@ def format_submodules(print_output=True):
     for line in out.split('\n'):
         cont = line.strip().split('=')
         if len(cont) < 2:
+            if line:
+                sys.stdout.write('Invalid line: %s\n' % line)
             continue
         mod = modules[index]
         mod['path'] = cont[1].strip()
         mod['name'] = mod['path'].split('/')[-1]
         mod['github_url'] = fmt % mod
-        mod['index'] = index
+        mod['destination'] = '%s/%s' % (installdir, mod['name'])
         index += 1
+
+    # sort according to name
+    modules.sort(key=lambda x: x['name'])
 
     # get commit hashes
     cmd = ('git submodule status')
     rc, out = run(cmd, can_fail=True)
 
+    index = 0
     for line in out.split('\n'):
         cont = line.strip().split(' ')
         if len(cont) < 3:
+            if line:
+                sys.stdout.write('Invalid line: %s\n' % line)
             continue
         for mod in modules:
             if mod['path'] == cont[1].strip():
+                mod['index'] = index
                 mod['commit'] = cont[0].strip()
+                url = mod['github_url'].replace('%%{%(name)s_commit}' % mod,
+                                                mod['commit'])
+                mod['download_url'] = url.replace('git://', 'https://')
+
+        index += 1
 
     # print output
     if print_output:
-        for fmt in ('%%global\t%(name)s_commit\t\t%(commit)s\n',
-                    'Source%(index)s:\t%(github_url)s\n'):
+        for head, fmt in [
+                ('Globals:\n',
+                 '%%global %(name)s_commit\t%(commit)s\n'),
+                ('Source list:\n',
+                 'Source%(index)s:\t%(github_url)s\n'),
+                ('Installation script:\n',
+                 'cp -r %(fullname)s-%%{%(name)s_commit} %(destination)s\n')]:
+            sys.stdout.write(head)
             for mod in modules:
-                sys.stdout.write(fmt % mod)
+                try:
+                    prefix, suffix = fmt.split('\t')
+                except Exception:
+                    sys.stdout.write(fmt % mod)
+                else:
+                    prefix = prefix % mod
+                    suffix = suffix % mod
+                    space = 30 - len(prefix) + len(suffix)
+                    sys.stdout.write(
+                        '%s %s' % (
+                            prefix, suffix.rjust(space)
+                        )
+                    )
+            sys.stdout.write('\n')
         sys.stdout.flush()
     return modules
 
 
 def download_submodules(modules, destination):
     for mod in modules:
-        url = mod['github_url'].replace('%%{%(name)s_commit}' % mod,
-                                        mod['commit'])
-        rc, out = run('wget %s' % url, can_fail=True, workdir=destination)
-        sys.stdout.write('Downloaded %(name)s (%(commit)s)\n' % mod)
+        rc, out = run('wget %(download_url)s' % mod,
+                      can_fail=True, workdir=destination)
+        if rc:
+            sys.stdout.write('Failed to download %(fullname)s (%(commit)s)\n' % mod)
+        else:
+            sys.stdout.write('Downloaded %(fullname)s (%(commit)s)\n' % mod)
     sys.stdout.flush()
 
 
@@ -87,6 +127,12 @@ class Git(ClientCommand):
                                             'submodules-download'
                              % self.normalized_name)
         # filtering options
+        self.parser.add_option('-i',  '--installdir',
+                               action='store',
+                               dest='installdir',
+                               default=None,
+                               help='path where modules will be installed')
+
         self.parser.add_option('-d',  '--destination',
                                action='store',
                                dest='destination',
@@ -99,12 +145,13 @@ class Git(ClientCommand):
         except ValueError:
             self.parser.error('Please specify command.')
 
+        installdir = kwargs.get('installdir')
         destination = kwargs.get('destination')
 
         # run subcommand
         if command == 'submodules-format':
-            format_submodules()
+            format_submodules(installdir)
 
         if command == 'submodules-download':
-            download_submodules(format_submodules(print_output=False),
-                                destination)
+            modules = format_submodules(installdir, print_output=False)
+            download_submodules(modules, destination)
